5c4da327ec2c594669fe96abb9f6d32c77fdfb0e
Charlie Mitchell <115580045+EndlessEndless1999@users.noreply.github.com>
2025-06-05 11:01:32 +0100

    add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[#1a1a1a] to-[#2e2e2e] text-white ">
+      <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[#1a1a1a] to-[#2e2e2e] text-white bg-no-repeat bg-cover" style={{ backgroundImage: "url('/rachel.gif')" }}>
         <div className="text-center border-white-50 p-4 border border-white rounded-md w-full max-w-md mx-auto mt-10">
           <h1 className="text-4xl font-light mb-4">Plaza</h1>
           <p className="text-sm text-gray-300">Now Playing: Ambient Dreams - DJ Calm</p>
