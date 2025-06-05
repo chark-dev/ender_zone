91205550579b1a686e2a8a04d043edcb95765af3
Charlie Mitchell <115580045+EndlessEndless1999@users.noreply.github.com>
2025-06-05 10:50:09 +0100

    add files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gradient-to-b from-[#1a1a1a] to-[#2e2e2e] text-white ">
+        <div className="text-center border-white-50 p-4 border border-white rounded-md w-full max-w-md mx-auto mt-10">
+          <h1 className="text-4xl font-light mb-4">Plaza</h1>
+          <p className="text-sm text-gray-300">Now Playing: Ambient Dreams - DJ Calm</p>
+          <audio controls autoPlay className="mt-6 text-center p-4 rounded-md w-full max-w-md mx-auto">
+            <source src="your-stream-url.mp3" type="audio/mpeg" />
+          </audio>
+        </div>
+      </div>
+      <div>
+        footer
+      </div>
+    </>
+  );
+}
+
